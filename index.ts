c1e6a2bdf66e58153a88fdb145cd306f57e1d4ef
Tom A. Vibeto <tom@onlyhuman.dk>
2020-05-17 16:05:12 +0200

    Serve other files.

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { listenAndServe } from 'https://deno.land/std/http/server.ts';
 import { acceptWebSocket, acceptable } from 'https://deno.land/std/ws/mod.ts';
+import { exists } from 'https://deno.land/std/fs/mod.ts';
 import { chat } from './chat.ts';
 
 listenAndServe({ port: 3000 }, async (req) => {
@@ -9,8 +10,9 @@ listenAndServe({ port: 3000 }, async (req) => {
 			headers: new Headers({
 				'content-type': 'text/html',
 			}),
-			body: await Deno.open('./index.html'),
+			body: await Deno.open('./public/index.html'),
 		});
+		return;
 	}
 
 	if (req.method === 'GET' && req.url === '/ws') {
@@ -22,6 +24,16 @@ listenAndServe({ port: 3000 }, async (req) => {
 				headers: req.headers,
 			}).then(chat);
 		}
+		return;
+	}
+
+	const url = './public' + req.url;
+	const fileExists = await exists(url);
+	if (fileExists) {
+		req.respond({
+			status: 200,
+			body: await Deno.open(url),
+		});
 	}
 });
 console.log('Server running on localhost:3000');
